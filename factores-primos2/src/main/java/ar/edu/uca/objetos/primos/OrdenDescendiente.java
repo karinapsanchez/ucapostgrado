b24e29741263ff22aa38967dc9d46aac7c115bc7
karinapsanchez <karinapsanchez@gmail.com>
2017-06-06 19:59:12 -0300

    Se agrego la version 3 de numeros primos.

---
+++ factores-primos2/src/main/java/ar/edu/uca/objetos/primos/OrdenDescendiente.java
@@ -0,0 +1,11 @@
+package ar.edu.uca.objetos.primos;
+
+public class OrdenDescendiente extends Orden{
+
+	public OrdenDescendiente(String tipo) {
+		super(tipo);
+	}
+	
+	
+
+}
--- factores-primos2/src/main/java/ar/edu/uca/objetos/primos/Programa.java
+++ factores-primos2/src/main/java/ar/edu/uca/objetos/primos/Programa.java
@@ -19,89 +19,62 @@ public class Programa {
      * @param args the command line arguments
      */
 	
-	private FormatoDefault formato;
+	
 	
 	
     public static void main(String[] args) {
   
     	if (args.length == 0) { //si no hay parámetros      
             System.out.println("Se deberá ingresar un numero");
-        }
-        	
-       if(args.length>0){
-           
-    	   List<Integer> factoresPrimos = obtenerFactorPrimo(Integer.parseInt(args[0]));
-    	   
-    	   if(args.length==2){
-    		   	   
-    		   Programa controlador = new Programa(new FormatoDefault(args[1]));
-    		   
-    		   controlador.formatoFactoresPrimos(factoresPrimos);         	    	
-    	   }  
-    	   
-       }
-       
-    }
-    
-    public Programa( FormatoDefault formato ){
-		this.formato = formato;
-	}
-    
-	public FormatoDefault getFormato() {
-		return formato;
-	}
+        }else{
+            List<Integer> factoresPrimos = obtenerFactoresPrimos(Integer.parseInt(args[0]));
+            
+            if (args.length > 0){
 
-	public void setFormato(FormatoDefault formato) {
-		this.formato = formato;
-	}
+            	ImprimeFactoresPrimos impFactoresPrimos = new ImprimeFactoresPrimos(new  Formato(args[1]));
+            	if(args.length == 3 && args[2].split("=")[0].toUpperCase().trim().equals("--OUTPUT-FILE")){
+            		impFactoresPrimos.imprimirArchivoFactoresPrimos(args[2].split("=")[1].toUpperCase().trim(), factoresPrimos,null);   
+            	}else if (args.length == 4 && args[2].split("=")[0].toUpperCase().trim().equals("--OUTPUT-FILE")){
+            		impFactoresPrimos.imprimirArchivoFactoresPrimos(args[2].split("=")[1].toUpperCase().trim(), factoresPrimos,args[3].split("=")[1].toUpperCase().trim());
+            	}
+            	if (args.length== 3 && args[2].split("=")[0].toUpperCase().trim().equals("--SORT")){
+            	   Orden orden = new Orden (args[2].split("=")[1]);
+                   impFactoresPrimos.imprimirFactoresPrimos(factoresPrimos, Integer.parseInt(args[0]), orden);
+            	}
+            	
+            }
+        }      
     
-    
-
-    private void formatoFactoresPrimos (List<Integer> factoresPrimos){
-    		
-    	String formatoResult = tipoFormato(formato.getFormato());
-    	
-    	System.out.println("Los numeros primos son: ");
-    	
-    	if (formatoResult.toUpperCase().equalsIgnoreCase("PRETTY")){
-    		
-    		formato.formatoDefault().imprimirFormato(factoresPrimos);
-    		
-    	}else{
-    		
-    		formato.formatoQuiet().imprimirFormato(factoresPrimos);
-    	}
-    	
-    	
     }
-    
-    private static String tipoFormato(String formato){
-    	
-    	String[] formatos = formato.split("=");
-    	
-    	return formatos[1];
-    	
-    }
-    
-    private static List<Integer> obtenerFactorPrimo(int num){
+    /*
+    * Obtiene una lista de los factores primos de un numero ingresado por el usuario
+    */
+    private static List<Integer> obtenerFactoresPrimos(int num){
         
         List<Integer> listaFactoresPrimos = new ArrayList<Integer>();
         
          int factorMinimo = 2;
+         
+           if(num== 1){
+               listaFactoresPrimos.add(1);
+           }else{
+                while(num >=factorMinimo){
+                    if(num%factorMinimo == 0){
+                        num /= factorMinimo;
+                  
+                        listaFactoresPrimos.add(factorMinimo);
+                  
+                    }else if (factorMinimo >2){
+                        factorMinimo +=2;
+                    }else{
+                        factorMinimo++;
+                    }
+                }
+ 
+           }
         
-           while(num >=factorMinimo){
-               
-              if(num%factorMinimo == 0){
-                  num /= factorMinimo;
-                  
-                  listaFactoresPrimos.add(factorMinimo);
-                  
-              }else if (factorMinimo >2){
-                  factorMinimo +=2;
-                  }else{
-                    factorMinimo++;
-                  }
-            }
+           
+           
         return listaFactoresPrimos;
     }
 
